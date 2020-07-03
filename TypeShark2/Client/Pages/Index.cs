1865b725d3d5eebfde8ddee5c1604bb30fb75301
Lee Richardson <github@leerichardson.com>
2020-07-03 11:40:30 -0400

    Extract player name retrieval into a child component

---
+++ TypeShark2/Client/Pages/Index.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using TypeShark2.Client.Data;
+
+namespace TypeShark2.Client.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IGameContext Context { get; private set; }
+
+        public void OnSetName(object obj)
+        {
+            // technically subscribing to the event on the child component is enough to call StateHasChanged, but for clarity:
+            StateHasChanged();
+        }
+    }
+}
--- TypeShark2/Client/Pages/PlayerNameComponent.cs
+++ TypeShark2/Client/Pages/PlayerNameComponent.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 using TypeShark2.Client.Data;
 
 namespace TypeShark2.Client.Pages
 {
-    public partial class Index
+    public partial class PlayerNameComponent
     {
         [Inject]
         public IGameContext Context { get; set; }
 
         public string TempName { get; set; }
 
-        public void SetName()
+        [Parameter]
+        public EventCallback OnSetName { get; set; }
+
+        public async Task SetName()
         {
             Context.PlayerName = TempName;
+            await OnSetName.InvokeAsync(null);
         }
     }
 }
